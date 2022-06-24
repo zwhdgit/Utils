7676430492722989ba3c8d7a2e1fdbbb3ff5b4f2
h.jn <zwhdyoux@163.com>
2022-06-24 18:41:19 +0800

    引入wifiP2p

---
+++ wifip2putil/src/main/java/com/zwh/wifip2putil/common/Constants.java
@@ -0,0 +1,13 @@
+package com.zwh.wifip2putil.common;
+
+/**
+ * @Author: leavesC
+ * @Date: 2019/11/23 17:54
+ * @Desc:
+ * @Github：https://github.com/leavesC
+ */
+public class Constants {
+
+    public static final int PORT = 1995;
+
+}
